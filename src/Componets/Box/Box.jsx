b0896897461ff9dd61b49bd5314e214c0029ce84
nilesh-catrak <pranetravyas3001@gmail.com>
2025-09-08 01:30:26 +0530

    added ig link

--- src/Componets/Box/Box.jsx
+++ src/Componets/Box/Box.jsx
@@ -10,12 +10,12 @@ function Box() {
                 <h1 className="mt-4 ml-4 text-6xl font-thin leading-none">Pranetra Vyas</h1>
                 <h1 className="mt-4 ml-4 text-sm font-thin leading-none">Full stack developer</h1>
 
-                <a href="/helloworld.docx" download className="mt-50 ml-4 text-[22px] font-thin leading-none hover:text-fuchsia-800 hover:text-[27px] duration-500">Projects</a>
+                <a className="mt-50 ml-4 text-[22px] font-thin leading-none hover:text-fuchsia-800 hover:text-[27px] duration-500">Projects</a>
                 <NavLink className="mt-5 ml-4 text-[22px] font-thin leading-none  hover:text-fuchsia-800 hover:text-[27px] duration-500">Education</NavLink>
-                <a href="/helloworld.docx" download target='_blank'className="mt-5 ml-4 text-[22px] font-thin leading-none  hover:text-fuchsia-800 hover:text-[27px] duration-500">Resume</a>
+                <a href="/resume_pranetravyas.docx" download target='_blank'className="mt-5 ml-4 text-[22px] font-thin leading-none  hover:text-fuchsia-800 hover:text-[27px] duration-500">Resume</a>
             </div>
             <div className="flex text-amber-50 mt-62 mr-4 font-thin gap-4 justify-end items-end">
-                <a href={""} target="_blank">
+                <a href={"https://www.instagram.com/pranetra4/"} target="_blank">
                     <Instagram className='hover:text-fuchsia-800 duration-300' />
                 </a>
                 <a href={"https://www.linkedin.com/in/pranetra-vyas-68b801343/"} target="_blank">
